7040bbb9b17d3927a829c1025ed034c0bec60fb7
Russell Johnson <blueyorange@gmail.com>
2023-09-24 05:27:00 +0100

    sync

--- build/index.js
+++ build/index.js
@@ -2,12 +2,14 @@ import { Marp } from "@marp-team/marp-core";
 import fs from "fs";
 import path from "path";
 import template from "./template.js";
+import chalk from 'chalk';
 
 // specify source and destination directories
-const sourceDirectory = 'src';
+const sourceDirectory = 'test';
 const targetDirectory = 'dist';
 
-const marp = new Marp();
+const marpOptions = { html: true };
+const marp = new Marp(marpOptions);
 
 async function processDirectory(sourceDir, targetDir) {
   const items = fs.readdirSync(sourceDir);
@@ -23,15 +25,11 @@ async function processDirectory(sourceDir, targetDir) {
       if (path.extname(item) === ".md") {
         // If the file has a ".md" extension, convert it to Marp Markdown and render as HTML
         const markdownContent = fs.readFileSync(sourcePath, "utf-8");
-        const marpOptions = { html: true };
-        const { html,css } = marp.render(markdownContent, marpOptions);
 
         const htmlFileName = path.basename(item, ".md") + ".html";
         const htmlTargetPath = path.join(targetDir, htmlFileName);
 
-        // You may need to replace this step with the actual rendering of the HTML
-        // Here, we assume you have a function or library to convert markdown to HTML
-        const renderedHtml = template({html, css});
+        const renderedHtml = template(marp.render(markdownContent));
 
         fs.writeFileSync(htmlTargetPath, renderedHtml);
       } else {
